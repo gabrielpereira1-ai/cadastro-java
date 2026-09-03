52ff662a80e75d1a64111358a071356045f8f8fa
gabrielpereira1-ai <gabriel.pereira1@acad.ifma.edu.br>
2026-09-03 08:24:49 -0300

    Create Cadastro.java

---
+++ Cadastro.java
@@ -0,0 +1,8 @@
+public class Cadastro {
+
+    public static void main(String[] args) {
+
+        System.out.println("Sistema de Cadastro");
+        System.out.println("Projeto conectado ao GitHub!");
+    }
+}
